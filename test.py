722e114d453eddb6e3c79b1d5fff0f5d9d43cf45
pussy_destroyer <dmitriyminaev9@gmail.com>
2025-03-27 09:46:51 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-# import re
-#
-# a = re.findall(r"\+7[ (-]?\d{3}\)?-? ?\d{3}[ -]?\d{2}[ -]?\d{2}", "+79895020964")
-# print(a)
-
-
-default_time = ["09-00", "12-00", "15-00", "18-00"]
-print(len(default_time))
